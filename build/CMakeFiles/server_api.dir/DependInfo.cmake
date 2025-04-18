1c091520ad2144000cfa04dea8e3254b2b9028cf
DKsheeraj <datta.ksheeraj@gmail.com>
2025-04-19 01:12:56 +0530

    Final

---
+++ build/CMakeFiles/server_api.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/datta/Documents/SEM-8/KGPKeeper/src/coordination.cpp" "CMakeFiles/server_api.dir/src/coordination.cpp.o" "gcc" "CMakeFiles/server_api.dir/src/coordination.cpp.o.d"
+  "/home/datta/Documents/SEM-8/KGPKeeper/src/server_api.cpp" "CMakeFiles/server_api.dir/src/server_api.cpp.o" "gcc" "CMakeFiles/server_api.dir/src/server_api.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/server_api.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/server_api.dir/src/coordination.cpp.o"
+  "CMakeFiles/server_api.dir/src/coordination.cpp.o.d"
+  "CMakeFiles/server_api.dir/src/server_api.cpp.o"
+  "CMakeFiles/server_api.dir/src/server_api.cpp.o.d"
+  "server_api"
+  "server_api.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/server_api.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
